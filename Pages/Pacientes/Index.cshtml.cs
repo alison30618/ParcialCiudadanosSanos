7c69ba590c421c1521299aea2a5b292b29420b58
alison30618 <alisonangulodiosa@gmail.com>
2023-11-19 16:56:33 -0500

    Index Pacientes

---
+++ Pages/Pacientes/Index.cshtml.cs
@@ -0,0 +1,26 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using ParcialCiudadanosSanos.Data;
+using ParcialCiudadanosSanos.Models;
+
+namespace ParcialCiudadanosSanos.Pages.Pacientes
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ParcialCiudadanosSanosContext _context;
+        public IndexModel(ParcialCiudadanosSanosContext context)
+        {
+            _context = context;
+        }
+        public IList<Paciente> Pacientes { get; set; } = default!;
+
+        public async Task OnGetAsync()
+        {
+            if (_context.Paciente != null)
+            {
+                Pacientes = await _context.Paciente.ToListAsync();
+            }
+        }
+    }
+}
