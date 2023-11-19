778d36321e71b0e6a59c558fd46c7e37e873cd43
alison30618 <alisonangulodiosa@gmail.com>
2023-11-19 16:40:26 -0500

    Agregar archivos de proyecto.

---
+++ Models/Paciente.cs
@@ -0,0 +1,12 @@
+﻿namespace ParcialCiudadanosSanos.Models
+{
+    public class Paciente
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int genero { get; set; }
+        public int NroDocumento { get; set; }
+        public long Contacto { get; set; }
+        
+    }
+}
